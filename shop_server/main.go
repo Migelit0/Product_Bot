b7ba8fee98a98d120ff1c5501aacbee2ef8faa34
Migelit0 <mm.panteleev@yandex.ru>
2021-12-08 17:48:13 +0400

    пока все иниты

--- shop_server/main.go
+++ shop_server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/jinzhu/gorm"
+	"os"
+	"github.com/joho/godotenv"
+	"fmt"
+) 
 
 func main() {
-	fmt.Println("Hello world")
+	e := godotenv.Load() //Загрузить файл .env
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+
+
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Создать строку подключения
+	fmt.Println(dbUri)
+
+	conn, err := gorm.Open("postgres", dbUri)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	db = conn
+	db.Debug().AutoMigrate(&Account{}, &Contact{}) //Миграция базы данных
+
 }
